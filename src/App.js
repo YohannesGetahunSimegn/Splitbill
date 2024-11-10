10fe8a85298962ce35d476a1a59dc95b4e95e8b5
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-11-10 16:11:36 +0300

    Form split bill done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,11 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add freind</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -60,7 +64,48 @@ function Friend({ friend }) {
         </p>
       )}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👭 Friend name</label>
+      <input type="text" />
+
+      <label>🌁ImageURL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰Bill value</label>
+      <input type="text" />
+
+      <label>👨‍🔬Your expense</label>
+      <input type="text" />
+
+      <label>👭 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Your friend</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
